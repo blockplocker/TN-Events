7c71d63cb15e8fbf1aa9d544e512afe7a4f11cce
blockplocker <noahallklint@gmail.com>
2026-04-10 11:23:13 +0200

    Create project

---
+++ Tn-Events/Tn-Events/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace Tn_Events.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ Tn-Events/Tn-Events/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace Tn_Events.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
