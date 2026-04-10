843c8819c6e3aad87b04d4bf8aa6a19e8d306127
Teddy Blomgren <Teddy_95@live.se>
2026-04-10 15:47:09 +0200

    Added missing required in ApplicationUser

--- Tn-Events/DAL/Data/ApplicationUser.cs
+++ Tn-Events/DAL/Data/ApplicationUser.cs
@@ -6,8 +6,8 @@ namespace DAL.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
 
         public ICollection<Booking> Bookings { get; set; }
     }
