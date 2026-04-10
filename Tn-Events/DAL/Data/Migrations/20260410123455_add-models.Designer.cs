97deb7fd18c7b05fce8f5e9b9515cfc8bbfd9eb6
Teddy Blomgren <Teddy_95@live.se>
2026-04-10 14:37:35 +0200

    Added first migration

--- Tn-Events/DAL/Data/Migrations/20260410123455_add-models.Designer.cs
+++ Tn-Events/DAL/Data/Migrations/20260410123455_add-models.Designer.cs
@@ -4,16 +4,19 @@ using DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace DAl.Migrations
+namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260410123455_add-models")]
+    partial class addmodels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
